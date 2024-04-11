8b90edc65f4f4ccd4bb2abc8c36c081d76388a9b
하석규 <haseoggyu@haseoggyuui-Macmini.local>
2024-04-11 23:18:09 +0900

    공부 6일차

---
+++ 모듈과 패키지/area.py
@@ -0,0 +1,8 @@
+# 상수는 대문자로 변경
+PI = 3.14
+
+def circle(radius : int) -> int:
+    return PI * radius * radius
+
+def squre(length : int) -> int:
+    return length * length
---
+++ 모듈과 패키지/run.py
@@ -0,0 +1,5 @@
+import area
+
+print(area.PI)
+print(area.circle(2))
+print(area.squre(3))
